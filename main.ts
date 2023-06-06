225a54d63669dd50d38d61174a7f8652355c883a
Wenceslao Negrete <vampay@digitaldealers.mx>
2023-06-06 01:09:39 -0600

    Initial commit w/ Typescript, react and electron

---
+++ main.ts
@@ -0,0 +1,34 @@
+const url = require("url");
+const { app, BrowserWindow } = require("electron");
+const path = require("path");
+// const handleOnSubmit = require("./handlers/on_submit");
+// const handleOnDrop = require("./handlers/on_drop");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 500,
+    height: 400,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  const startUrl =
+    process.env.ELECTRON_START_URL ||
+    url.format({
+      pathname: path.join(__dirname, "/../build/index.html"),
+      protocol: "file:",
+      slashes: true,
+    });
+  mainWindow.loadURL(startUrl);
+
+  //Enable when developing
+  mainWindow.openDevTools();
+
+  // handleOnSubmit();
+  // handleOnDrop();
+}
+
+app.on("ready", createWindow);
